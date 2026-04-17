fd3a084fde5a9841cbf1e2b4969bfb6d49ee1e3e
Salau Aisha Omowunmi <salauaisha53@gmail.com>
2026-04-18 00:12:56 +0100

    Fix auth and transaction controllers

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const connectDB = require('./src/config/db');
 const app = require('./src/app');
 const PORT = process.env.PORT; 
 connectDB();
-app.get("/test", (req, res) => {
-  res.json({ message: "API works" });
-});
 app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
 });
